75f819a4e94f5d65302f2dbe884b890c784d7ead
Saipuneethg <saipuneeth2005@gmail.com>
2026-03-29 20:32:31 +0530

    updated changes

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ===================================================
-echo Installing required packages (Flask)...
-pip install flask
-echo.
-echo Starting the Birthday Website Server...
-echo ===================================================
-python app.py
-pause
